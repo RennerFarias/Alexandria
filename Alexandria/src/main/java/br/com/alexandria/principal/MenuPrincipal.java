7f8e9998c92eb1b2e9fcc771c1bf36d1727731b4
Elian Barros <elian.b.siqueira@gmail.com>
2026-06-12 15:32:54 -0300

    Update MenuPrincipal.java
    Adicionado loop ao menu principal

---
+++ Alexandria/src/main/java/br/com/alexandria/principal/MenuPrincipal.java
@@ -0,0 +1,84 @@
+package br.com.alexandria.principal;
+
+import javax.swing.*;
+
+public class MenuPrincipal {
+    public void exibirMenu() {
+        boolean continuar = true;
+        boolean continuarOpcao = true;
+
+        do {
+            continuarOpcao = true;
+            String[] opcoes = {"Funcionários", "Alunos", "Sair"};
+            JTextField campoUsuario = new JTextField();
+            JPasswordField campoSenha = new JPasswordField();
+
+            Object[] componentesLogin = {"Usuário: ", campoUsuario, "Senha: ", campoSenha};
+
+            String usuario;
+            String senha;
+
+            int opcaoEscolhida = JOptionPane.showOptionDialog
+                    (null, "Qual é o seu perfil de acesso?",
+                            "Perfil de acesso", JOptionPane.DEFAULT_OPTION,
+                            JOptionPane.QUESTION_MESSAGE, null, opcoes, null);
+
+            if (opcaoEscolhida == 0) {
+                do {
+                    // Login funcionario
+                    int opcaoClicada = JOptionPane.showConfirmDialog(null, componentesLogin,
+                            "Acesso ao sistema",  JOptionPane.OK_CANCEL_OPTION,  JOptionPane.QUESTION_MESSAGE);
+
+                    if (opcaoClicada == JOptionPane.OK_OPTION) {
+                        usuario = campoUsuario.getText();
+                        senha = new String(campoSenha.getPassword());
+
+                        // Bloco para passar as informacoes para a classe conexao
+                        // JOptionPane.showMessageDialog(null, "Usuário ou senha incorretos!", "Erro de Acesso",
+                        // JOptionPane.ERROR_MESSAGE);
+
+                        MenuFuncionario menuFuncionario = new MenuFuncionario();
+                        menuFuncionario.exibirMenu();
+
+                    }  else {
+                        JOptionPane.showMessageDialog(null, "Login cancelado pelo usuario");
+                        continuarOpcao = false;
+                    }
+                } while (continuarOpcao);
+
+
+            } else if (opcaoEscolhida == 1) {
+                do {
+                    // Login Aluno
+                    int opcaoClicada = JOptionPane.showConfirmDialog(null, componentesLogin,
+                            "Acesso ao sistema",  JOptionPane.OK_CANCEL_OPTION,  JOptionPane.QUESTION_MESSAGE);
+
+                    if (opcaoClicada == JOptionPane.OK_OPTION) {
+                        usuario = campoUsuario.getText();
+                        senha = new String(campoSenha.getPassword());
+
+                        // Bloco para passar as informacoes para a classe conexao
+                        // JOptionPane.showMessageDialog(null, "Usuário ou senha incorretos!", "Erro de Acesso",
+                        // JOptionPane.ERROR_MESSAGE);
+
+                        MenuAluno menuAluno = new MenuAluno();
+                        menuAluno.exibirMenu();
+
+                    }  else {
+                        JOptionPane.showMessageDialog(null, "Login cancelado pelo usuario");
+                        continuarOpcao = false;
+                    }
+                } while (continuarOpcao);
+
+
+            } else  {
+
+                continuar = false;
+            }
+        } while (continuar);
+        JOptionPane.showMessageDialog(null, "Saindo...");
+
+
+
+    }
+}
